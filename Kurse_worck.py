294e13175d732a9eaa3f9aa4a4370a7d6cc41cbc
Sarmat-K <sarmat.karaf.0@gmail.com>
2023-04-23 18:07:07 +0300

    upd

--- Kurse_worck.py
+++ Kurse_worck.py
@@ -1,81 +1,79 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-from torchvision.models.detection import fasterrcnn_resnet50_fpn
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
 
 # Define the neural network architecture
-def get_model(num_classes):
-    # load an instance segmentation model pre-trained on COCO
-    model = fasterrcnn_resnet50_fpn(pretrained=True)
-    
-    # replace the classifier with a new one, that has
-    # num_classes which is user-defined
-    num_classes = num_classes  # 1 class (person) + background
-    # get number of input features for the classifier
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # replace the pre-trained head with a new one
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+class FasterRCNNNet(nn.Module):
+    def __init__(self):
+        super(FasterRCNNNet, self).__init__()
+        self.backbone = resnet_fpn_backbone('resnet50', pretrained=True)
+        self.rpn = nn.Conv2d(in_channels=self.backbone.out_channels, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.head = FastRCNNPredictor(self.backbone.out_channels, num_classes=2)
 
-    return model
+    def forward(self, x):
+        features = self.backbone(x)
+        proposals, _ = self.rpn(features)
+        detections = self.head(features, proposals)
+        return detections, {}
+
 
 # Define the dataset and data loader
-def get_data_loader(train_folder, test_folder, batch_size):
-    transform = transforms.Compose([
-        transforms.Resize((800, 800)),
-        transforms.ToTensor(),
-    ])
-    train_dataset = datasets.CocoDetection(
-        train_folder, annFile=train_folder+'/annotations/instances_train2017.json', transform=transform)
-    test_dataset = datasets.CocoDetection(
-        test_folder, annFile=test_folder+'/annotations/instances_val2017.json', transform=transform)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
-    return train_loader, test_loader
+def collate_fn(batch):
+    return tuple(zip(*batch))[0]  # Return only the image data
 
-# Instantiate the neural network and define the loss function and optimizer
-model = get_model(num_classes=2)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0005)
 
-# Define the COCO dataset classes
-coco_classes = [
-    '__background__', 'person'
-]
+if __name__ == '__main__':
+    # Define the paths to the COCO dataset
+    train_data = datasets.CocoDetection(root='C:/Users/lolol/OneDrive/Документы/СOCO Dataset/train2017', annFile='C:/Users/lolol/OneDrive/Документы/СOCO Dataset/annotations/instances_train2017.json', transform=transforms.ToTensor())
+    test_data = datasets.CocoDetection(root='C:/Users/lolol/OneDrive/Документы/СOCO Dataset/test2017', annFile='C:/Users/lolol/OneDrive/Документы/СOCO Dataset/annotations/instances_val2017.json', transform=transforms.ToTensor())
 
-# Train the neural network
-train_loader, test_loader = get_data_loader('path/to/train/folder', 'path/to/test/folder', batch_size=2)
-for epoch in range(10):
-    for batch_idx, (data, targets) in enumerate(train_loader):
-        data = [d.to(device) for d in data]
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+    # Define the data loader
+    train_loader = DataLoader(train_data, batch_size=8, shuffle=True, num_workers=4, collate_fn=collate_fn)
+    test_loader = DataLoader(test_data, batch_size=8, shuffle=False, num_workers=4, collate_fn=collate_fn)
 
-        # Forward pass
-        loss_dict = model(data, targets)
-        losses = sum(loss for loss in loss_dict.values())
-        
-        # Backward pass and optimization
-        optimizer.zero_grad()
-        losses.backward()
-        optimizer.step()
-        
-        # Print training progress
-        if batch_idx % 100 == 0:
-            print(f'Epoch [{epoch+1}/{10}], Batch [{batch_idx}/{len(train_loader)}], Loss: {losses.item():.4f}')
-    
-    # Evaluate the neural network on the test set
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        for data, targets in test_loader:
-            data = [d.to(device) for d in data]
-            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-            
-            model.eval()
-            outputs = model(data)
+    # Instantiate the neural network and define the loss function and optimizer
+    model = FasterRCNNNet()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    # Train the neural network
+    for epoch in range(10):
+        print(1)
+        for batch_idx, images in enumerate(train_loader):  # images only, not targets
+            print(2)
+            # Forward pass
+            detections, _ = model(images)
+            loss = sum(loss for loss in detections.values())
+
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            print(f'Epoch [{epoch+1}/{10}], Batch [{batch_idx}/{len(train_loader)}], Loss: {loss.item():.4f}')
+            # Print training progress
+            if batch_idx % 100 == 0:
+
+                print(f'Epoch [{epoch+1}/{10}], Batch [{batch_idx}/{len(train_loader)}], Loss: {loss.item():.4f}')
+
+        # Evaluate the neural network on the test set
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for images, targets in test_loader:
+                detections, _ = model(images, targets)
+                for detection in detections:
+                    if detection['labels'].item() == 1:  # person class
+                        correct += 1
+                total += len(targets)
+            accuracy = 100 * correct / total
+            print(f'Test Accuracy: {accuracy:.2f}%')
+
+    # Save the trained model
+    torch.save(model.state_dict(), 'FasterRCNNNet_model.pth')
+    print('Model saved!')
